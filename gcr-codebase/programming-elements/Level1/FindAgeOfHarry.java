e2775abff1f343301f05a1c1759e1a9403d0ea37
rd-sharma9 <srudresh815@gmail.com>
2025-12-17 12:08:40 +0530

    [Rudresh] : Added FindAgeOfHarry

---
+++ gcr-codebase/programming-elements/Level1/FindAgeOfHarry.java
@@ -0,0 +1,32 @@
+/* 
+
+Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
+I/P => NONE
+O/P => Harry's age in 2024 is ___ 
+
+*/
+
+
+
+import java.util.Scanner;
+
+public class FindAgeOfHarry{
+
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+		
+		int birthYear = 2000;
+		int currentYear = 2024;
+		
+		int age = currentYear - birthYear;	
+		
+		System.out.println("Harry's age in 2024 is " + age);
+
+		
+		input.close();
+
+	}
+
+
+}
