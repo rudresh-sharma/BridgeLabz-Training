19027d77facf9ba0ac6f4b3e2d5a19ade8dd5dfe
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2025-12-25 13:02:41 +0530

    [Rudresh] : Added Elements&DigitSum.java file

---
+++ leetcode-codebase/Arrays/Practice1/Elements&DigitSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for (int num : nums) {
+            elementSum += num;
+
+            int n = num;
+            while (n > 0) {
+                digitSum += n % 10;
+                n /= 10;
+            }
+        }
+
+        return Math.abs(elementSum - digitSum);
+    }
+}
