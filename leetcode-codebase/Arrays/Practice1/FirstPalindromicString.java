881e196290277937b0efbb2cef974a860663b448
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2025-12-25 12:54:43 +0530

    [Rudresh] : Added FirstPalindromic.java file

---
+++ leetcode-codebase/Arrays/Practice1/FirstPalindromicString.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        String pdStr = "";
+        
+        for(int i=0; i<words.length; i++){
+            if(isPalindrome(words[i])){
+                pdStr = words[i];
+                break;
+            }
+        }
+
+        return pdStr;
+    }
+
+
+
+    public static boolean isPalindrome(String str){
+
+        int left = 0;
+        int right = str.length()-1;
+
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
