d7326c530b3ecf3918f5233addeabe8f41c85f2b
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2025-12-27 21:52:31 +0530

    [Rudresh] : Added CircleMain.java program file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/CircleMain.java
@@ -0,0 +1,15 @@
+package com.constructors.levelone;
+
+public class CircleMain {
+
+	
+	
+	public static void main(String[] args) {
+		
+		//creating circle object and pringint radius and area
+		Circle c1 = new Circle();				
+		System.out.println("Area of radius " + c1.getRadius() + " is " + c1.area());
+		
+	}
+	
+}
