e10e62211b40fe303dc88f3c71a0fd634fa7eb9c
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2025-12-30 21:52:11 +0530

    [Rudresh] Added DepartmentUniversity.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/DepartmentUniversity.java
@@ -0,0 +1,15 @@
+package com.objectmodeling;
+
+public class DepartmentUniversity {
+	    private String deptName;
+
+	    public DepartmentUniversity(String deptName) {
+	        this.deptName = deptName;
+	    }
+
+	    public void showDepartment() {
+	        System.out.println("Department: " + deptName);
+	    }
+	
+
+}
