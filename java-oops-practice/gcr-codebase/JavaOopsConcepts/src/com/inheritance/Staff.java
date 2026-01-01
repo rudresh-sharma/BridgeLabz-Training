9197e803ab3235bda0f4035d873883ab25057fa9
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-01 09:51:12 +0530

    [Rudresh] :Added Staff.java program file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/Staff.java
@@ -0,0 +1,16 @@
+package com.inheritance;
+
+public class Staff extends Person {
+
+    private String department;
+
+    public Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    public void displayRole() {
+        System.out.println("Role : Staff");
+        System.out.println("Department : " + department);
+    }
+}
