64ad22d37b36fd8ce1681152e83ec5b35858d884
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 12:21:19 +0530

    [Rudresh] : Added LifeCycle (JUNIT)

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/junit/lifecycle/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package com.collections.junit.lifecycle;
+
+public class DatabaseConnection {
+
+    private boolean connected = false;
+
+    public void connect() {
+        connected = true;
+    }
+
+    public void disconnect() {
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
