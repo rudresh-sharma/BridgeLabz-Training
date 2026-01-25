eb08fec6e8193e01e1f6e7851ee33dca83cd7647
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 10:46:21 +0530

    [Rudresh] :Added SetToSortedList.java program file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/setinterface/SetToSortedList.java
@@ -0,0 +1,31 @@
+package com.collections.setinterface.settosortedlist;
+import java.util.*;
+
+public class SetToSortedList {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input Set
+        System.out.print("Enter number of elements in the set: ");
+        int n = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        // Convert to List
+        List<Integer> sortedList = new ArrayList<>(set);
+
+        // Sort List
+        Collections.sort(sortedList);
+
+        // Output
+        System.out.println("\nSorted List: " + sortedList);
+
+        sc.close();
+    }
+}
