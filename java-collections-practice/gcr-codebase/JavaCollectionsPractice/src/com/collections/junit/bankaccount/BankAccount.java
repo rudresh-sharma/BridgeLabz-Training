80a2de807ad98a9c86bc0381aea24ac3d6dc3cb0
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 12:19:38 +0530

    [Rudresh] : Added BankAccount (JUNIT)

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/junit/bankaccount/BankAccount.java
@@ -0,0 +1,24 @@
+package com.collections.junit.bankaccount;
+
+public class BankAccount {
+
+    private double balance;
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException();
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/junit/bankaccount/BankAccountTest.java
@@ -0,0 +1,29 @@
+package com.collections.junit.bankaccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    @Test
+    void testDeposit() {
+        BankAccount account = new BankAccount();
+        account.deposit(1000);
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        BankAccount account = new BankAccount();
+        account.deposit(1000);
+        account.withdraw(400);
+        assertEquals(600, account.getBalance());
+    }
+
+    @Test
+    void testWithdrawInsufficientFunds() {
+        BankAccount account = new BankAccount();
+        account.deposit(300);
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(500));
+    }
+}
