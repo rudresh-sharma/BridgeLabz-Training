ef12c121b9d2e0455300a90790dce20653d34ade
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 12:39:40 +0530

    [Rudresh] : Added Temperature (JUNIT)

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/junit/temperature/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package com.collections.junit.temperature;
+
+public class TemperatureConverter {
+
+    public double celsiusToFahrenheit(double c) {
+        return (c * 9 / 5) + 32;
+    }
+
+    public double fahrenheitToCelsius(double f) {
+        return (f - 32) * 5 / 9;
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/junit/temperature/TemperatureConverterTest.java
@@ -0,0 +1,19 @@
+package com.collections.junit.temperature;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TemperatureConverterTest {
+
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(98.6, converter.celsiusToFahrenheit(37), 0.1);
+    }
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.1);
+    }
+}
