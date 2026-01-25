b2f35d1d18c76b2f6851f46c0513fdfc517a3ae8
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 11:00:51 +0530

    [Rudresh] :Added RepeatingWordsFinder.java program file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/regex/RepeatingWordsFinder.java
@@ -0,0 +1,17 @@
+package com.collections.regex;
+
+import java.util.regex.*;
+
+public class RepeatingWordsFinder {
+
+    public static void main(String[] args) {
+        String text = "This is is a repeated repeated word test.";
+
+        Pattern pattern = Pattern.compile("\\b(\\w+)\\s+\\1\\b", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
+    }
+}
