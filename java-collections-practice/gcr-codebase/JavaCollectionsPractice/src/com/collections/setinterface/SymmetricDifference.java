5814e5d406f83e4b126bc391885bebd1341af4a2
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-25 10:46:40 +0530

    [Rudresh] :Added SymmetricDifference.java program file

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/setinterface/SymmetricDifference.java
@@ -0,0 +1,44 @@
+package com.collections.setinterface.symmetricdifference;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SymmetricDifference {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input Set 1
+        System.out.print("Enter number of elements in Set 1: ");
+        int n1 = sc.nextInt();
+        Set<Integer> set1 = new HashSet<>();
+        System.out.println("Enter elements of Set 1:");
+        for (int i = 0; i < n1; i++) {
+            set1.add(sc.nextInt());
+        }
+
+        // Input Set 2
+        System.out.print("\nEnter number of elements in Set 2: ");
+        int n2 = sc.nextInt();
+        Set<Integer> set2 = new HashSet<>();
+        System.out.println("Enter elements of Set 2:");
+        for (int i = 0; i < n2; i++) {
+            set2.add(sc.nextInt());
+        }
+
+        // Compute Symmetric Difference
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2); // A ∪ B
+
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2); // A ∩ B
+
+        union.removeAll(intersection); // (A ∪ B) - (A ∩ B)
+
+        // Output
+        System.out.println("\nSymmetric Difference: " + union);
+
+        sc.close();
+    }
+}
