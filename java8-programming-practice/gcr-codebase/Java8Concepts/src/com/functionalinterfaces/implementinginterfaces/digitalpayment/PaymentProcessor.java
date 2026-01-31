4d6ed081faa0f2575eb6642607090b1af31372cd
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-01-31 22:46:26 +0530

    [Rudresh] : Added Digital Payment (Functional Interface Question)

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/implementinginterfaces/digitalpayment/PaymentProcessor.java
@@ -0,0 +1,6 @@
+package com.functionalinterfaces.implementinginterfaces.digitalpayment;
+
+public interface PaymentProcessor {
+    // processes the payment of given amount
+    void pay(double amount);
+}
